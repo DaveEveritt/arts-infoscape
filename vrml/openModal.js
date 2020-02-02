b5a52de7a02c615af82ec6c07f3e68dc7dae4509
Dave Everitt <deveritt@innotts.co.uk>
2020-02-02 14:05:16 +0000

    temporary open page for People Express

--- vrml/openModal.js
+++ vrml/openModal.js
@@ -1,1 +1,6 @@
-document.window.open('./data/566.html','details','height=550,width=400,top=50,left=50,menubar=no,status=no,toolbar=no,titlebar=no');
+function showInfo(info) {
+  console.log(`./data/${info}.html`);
+  // document.window.open(`./data/${info}.html`,'details','height=550,width=400,top=50,left=50,menubar=no,status=no,toolbar=no,titlebar=no');
+}
+
+
